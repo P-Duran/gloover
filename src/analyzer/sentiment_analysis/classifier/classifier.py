3161b5719b4cd91d58d4ee1a48fe8f0ce6e9d085
P-Duran <pablodurandelasheras@live.com>
2021-01-25 21:28:41 +0100

    Added save model

--- src/analyzer/sentiment_analysis/classifier/classifier.py
+++ src/analyzer/sentiment_analysis/classifier/classifier.py
@@ -12,27 +12,40 @@ import numpy as np
 from sklearn.pipeline import FeatureUnion, Pipeline
 from analyzer.sentiment_analysis.classifier.features.features import NegateWordsContext, PercentageContextNegative, PosTagCounter, TotalSentimentScore, WordsExtractor
 from sklearn.metrics import f1_score, precision_score, recall_score
+import joblib
 import sys
+import os
+import errno
+
+DEFAULT_MODEL_PATH = 'resources/models/classifierModel.mdl'
+DEFAULT_DATASET_PATH = 'resources/datasets/reviews_Cell_Phones_and_Accessories_5.json'
 
 class Classifier:
+
     
-    def __init__(self, data_set_path, test_size = 0.6, train_size = 0.4):
-        self.data = self.__load_training_data__(data_set_path)
-        self.data_train, self.data_test, self.y_train, self.y_true=train_test_split(self.data['text'], self.data['polarity'], test_size=test_size, train_size=train_size)
-        self.model = self.__load_model__()
-    
+    def __init__(self, dataset_path = DEFAULT_DATASET_PATH, model_path = DEFAULT_MODEL_PATH, train_size = 0.6, test_size = 0.4):
+        self.dataset_path = dataset_path
+        self.model_path = model_path
+        self.data = self.__load_training_data__(dataset_path)
+        self.data_train, self.data_test, self.y_train, self.y_true = train_test_split(
+            self.data['text'], self.data['polarity'], test_size=test_size, train_size=train_size)
+        self.model = self.__load_model__(model_path)
+
     def test_model(self):
-        y_test=self.model.predict(self.data_test)
+        y_test = self.model.predict(self.data_test)
         return sklearn.metrics.accuracy_score(self.y_true, y_test)
 
     def classify(self, reviews):
-        return self.model.predict(reviews)
+        return self.model.predict(pd.Series(reviews))
 
+    def __save_model__(self,model): 
+        os.makedirs(os.path.dirname(self.model_path), exist_ok=True)
+        joblib.dump(model, self.model_path)
 
-    def __load_training_data__(self,path):
-        print('Hello world!', file=sys.stderr)
+    def __load_training_data__(self, path=None):
         data = pd.read_json(path, lines=True)
-        data.columns = ['reviewerID','asin','reviewerName','helpful','text','polarity','summary','unixReviewTime','reviewTime']
+        data.columns = ['reviewerID', 'asin', 'reviewerName', 'helpful',
+                        'text', 'polarity', 'summary', 'unixReviewTime', 'reviewTime']
         data.polarity[data.polarity < 2.5] = -1
         data.polarity[data.polarity >= 2.5] = 1
         data = data[(data.polarity == 1) | (data.polarity == -1)]
@@ -43,86 +56,36 @@ class Classifier:
         remove = np.random.choice(labels.iloc[0], excess, replace=False)
         df2 = data[~data.text.isin(remove)]
         return df2
-    
-    def __load_model__(self):
-        ppl_neg = Pipeline([('_NEG words', NegateWordsContext()),
-                        ('word_ngrams', CountVectorizer(ngram_range=(1, 4), analyzer='word')), ])
-        ppl = Pipeline([
-            ('text_features', FeatureUnion([
-                ('postag', PosTagCounter()),
-                # ('v', Pipeline(
-                #     [('efe', WordsExtractor()), ('ge', CountVectorizer(ngram_range=(1, 4), analyzer='char'))])),
-                ('char_ngrams', CountVectorizer(ngram_range=(1, 4), analyzer='char')),
-                ('neg', ppl_neg),
-                ('tfidf', TfidfVectorizer()),
-                ('%', PercentageContextNegative()),
-                ('ss', TotalSentimentScore()),
 
-            ])),
-            ('clf',   LinearSVC())
-        ])
-        model=ppl.fit(self.data_train, self.y_train)
-        return model
+    def __load_model__(self,model_path = None):
+        try:
+            print('Loading model...')
+            return joblib.load(model_path)
+        except Exception as e:
+            print('Problem loading the model, a new one would be trained')
+            ppl_neg = Pipeline([('_NEG words', NegateWordsContext()),
+                                ('word_ngrams', CountVectorizer(ngram_range=(1, 4), analyzer='word')), ])
+            ppl = Pipeline([
+                ('text_features', FeatureUnion([
+                    ('postag', PosTagCounter()),
+                    # ('v', Pipeline(
+                    #     [('efe', WordsExtractor()), ('ge', CountVectorizer(ngram_range=(1, 4), analyzer='char'))])),
+                    ('char_ngrams', CountVectorizer(
+                        ngram_range=(1, 4), analyzer='char')),
+                    ('neg', ppl_neg),
+                    ('tfidf', TfidfVectorizer()),
+                    ('%', PercentageContextNegative()),
+                    ('ss', TotalSentimentScore()),
+
+                ])),
+                ('clf',   LinearSVC())
+            ])
+            model = ppl.fit(self.data_train, self.y_train)
+            self.__save_model__(model)
+            return model
+
 
 if __name__ == "__main__":
-    ''
-    # data = pd.read_json(
-    #     'resources/datasets/reviews_Cell_Phones_and_Accessories_5.json', lines=True)
-    # data.columns = ['asin', 'helpful', 'polarity', 'text', 'reviewTime', 'reviewerID',
-    #                 'reviewerName', 'summary', 'unixReviewTime']
-    # data.polarity[data.polarity < 2.5] = -1
-    # data.polarity[data.polarity >= 2.5] = 1
-
-    # data = data[(data.polarity == 1) | (data.polarity == -1)]
-    # # data = pd.read_csv(
-    # #     'resources/datasets/Reddit_Data.csv', encoding="utf-8")
-    # # data.columns = ['text', 'polarity']
-    # # data = data[data.polarity != 0]
-    # labels = data.groupby('polarity').text.unique()
-    # # Sort the over-represented class to the head.
-    # labels = labels[labels.apply(len).sort_values(ascending=False).index]
-    # excess = len(labels.iloc[0]) - len(labels.iloc[1])
-    # remove = np.random.choice(labels.iloc[0], excess, replace=False)
-    # df2 = data[~data.text.isin(remove)]
-
-    # data_len = len(data)
-    # ppl_neg = Pipeline([('_NEG words', NegateWordsContext()),
-    #                     ('word_ngrams', CountVectorizer(ngram_range=(1, 4), analyzer='word')), ])
-    # ppl = Pipeline([
-    #     ('text_features', FeatureUnion([
-    #         ('postag', PosTagCounter()),
-    #         # ('v', Pipeline(
-    #         #     [('efe', WordsExtractor()), ('ge', CountVectorizer(ngram_range=(1, 4), analyzer='char'))])),
-    #         ('char_ngrams', CountVectorizer(ngram_range=(1, 4), analyzer='char')),
-    #         ('neg', ppl_neg),
-    #         ('tfidf', TfidfVectorizer()),
-    #         ('%', PercentageContextNegative()),
-    #         ('ss', TotalSentimentScore()),
-
-    #     ])),
-    #     ('clf',   LinearSVC())
-    # ])
-
-    # data_train, data_test, y_train, y_true=train_test_split(
-    #     df2['text'], df2['polarity'], test_size=0.1, train_size=0.1)
-    # model=ppl.fit(data_train, y_train)
-    # y_test=model.predict(data_test)
-    # print(y_test)
-    # newvals=pd.Series([ "There were few scratches on the frame and the plastic is poor quality after removing the screws they are barely can be screwed in second time. The instructions are misleading - there are probably other bike models available and some of the assembly instruction don't apply to the model we bought.overall quality is less than average ",
-    #                     ]
-    #                     )
-    # result=model.predict(newvals)
-    # print(sklearn.metrics.accuracy_score([-1],
-    #                                      result))
-    # print(sklearn.metrics.accuracy_score(y_true, y_test))
-    # print(f1_score(y_test, y_true, average="macro"))
-    # print(precision_score(y_test, y_true, average="macro"))
-    # print(recall_score(y_test, y_true, average="macro"))
-    # # plot_decision_regions(X=X.values,
-    # #                       y=y.values,
-    # #                       clf=m1,
-    # #                       legend=2)
-
-    # # Update plot object with X/Y axis labels and Figure Titl
-    # # plt.xlabel(X.columns[0], size=14)
-    # # plt.title('SVM Decision Region Boundary', size=16)
+    classifier = Classifier(
+        'resources/datasets/reviews_Cell_Phones_and_Accessories_5.json', test_size=0.05, train_size=0.05)
+    print(classifier.classify(['This is the best']))
--- src/server/app.py
+++ src/server/app.py
@@ -5,9 +5,9 @@ import pandas as pd
 from analyzer.sentiment_analysis.classifier.classifier import Classifier
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
-classifier = Classifier('resources/datasets/reviews_Cell_Phones_and_Accessories_5.json',test_size=0.1,train_size=0.1)
+classifier = Classifier(test_size=0.05,train_size=0.05)
 def get_hit_count():
-    retries = 5
+    retries = 5 
     while True:
         try:
             return cache.incr('hits')
