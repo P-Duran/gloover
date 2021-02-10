1f6b8ccbc37531452a5dbeec609688f46126f81c
Pablo Durán <71401791+P-Duran@users.noreply.github.com>
2021-02-10 21:07:25 +0100

    Merge Feature/#20 create a scrapper to get reviews from any source into develop (#35)
    * Added scrapper
    
    * added inits to watch for packages
    
    * Scrapper with selectorLib working nicely
    
    Co-authored-by: Pablo Durán <pduran@corunet.com>

---
+++ gloover_model/scraper/spiders/amazon_spider.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import re
+
+import scrapy
+import os
+import selectorlib
+from dateutil import parser as dateparser
+
+
+class AmazonSpider(scrapy.Spider):
+    _COUNTRY_DATE_PATTERN = '.* in (the )?(?P<country>[a-zA-Z ]*) on (?P<date>.*)'
+    _POLARITY_PATTERN = '(?P<polarity>[0-9//.]+)[a-z ]*(?P<maxPolarity>[0-9//.]+)[a-z ]*'
+
+    name = 'amazon'
+    allowed_domains = ['amazon.com']
+    start_urls = [
+        'https://www.amazon.com/HP-24mh-FHD-Monitor-Built/product-reviews/B08BF4CZSV/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews']
+
+    product_page_extractor = selectorlib.Extractor.from_yaml_file(
+        os.path.join(os.path.dirname(__file__), '../configurations/amazon/review.yml'))
+
+    def parse(self, response):
+        # Extract data using Extractor
+        data = self.product_page_extractor.extract(response.text)
+
+        if 'next_page' in data:
+            yield scrapy.Request("https://www.amazon.com" + data['next_page'], callback=self.parse,)
+        if 'reviews' in data:
+            for review in data['reviews']:
+                yield from self.create_review(data['product_title'], review)
+
+    def create_review(self, product_name, unprocessed_review):
+        polarity_match = re.search(self._POLARITY_PATTERN, unprocessed_review['rating'])
+        country_date_match = re.search(self._COUNTRY_DATE_PATTERN, unprocessed_review['date'])
+        try:
+            country = country_date_match.group('country')
+            date = dateparser.parse(country_date_match.group('date')).strftime('%d %b %Y')
+            print(country, date)
+        except Exception:
+            country = unprocessed_review['date']
+            date = unprocessed_review['date']
+        try:
+            polarity = polarity_match.group('polarity')
+            max_polarity = polarity_match.group('maxPolarity')
+        except Exception:
+            polarity = unprocessed_review['rating']
+            max_polarity = unprocessed_review['rating']
+
+        product_review = {'productName': product_name, 'title': unprocessed_review['title'],
+                          'text': unprocessed_review['content'], 'date': date, 'country': country,
+                          'author': unprocessed_review['author'], 'polarity': polarity, 'maxPolarity': max_polarity}
+        print(product_review)
+        yield product_review
