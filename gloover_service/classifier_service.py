2dab6d13a20c12fefa0916e3809204e72ab01a91
pablo <pablodurandelasheras@live.com>
2021-03-28 12:47:09 +0200

    Added endoints to reatrain the classifier, manage models...

--- gloover_service/classifier_service.py
+++ gloover_service/classifier_service.py
@@ -1,3 +1,6 @@
+import glob
+import pathlib
+import re
 from typing import List
 
 from gloover_model.classifier import Classifier
@@ -8,6 +11,10 @@ classifier = Classifier()
 
 
 class ClassifierService:
+    _MODEL_REGEX = "([^/]+/)*(?P<id>[^/]+).mdl"
+    _MODELS_DIR = "resources/models"
+    _MODEL_CONTAINER_REGEX = _MODELS_DIR + "/*.mdl"
+
     def __init__(self):
         if classifier.model is None:
             self._initialize_classifier()
@@ -15,15 +22,49 @@ class ClassifierService:
     def classify(self, texts: List[str]):
         return classifier.classify(texts)
 
-    def create_model_from_database(self, asin):
+    def get_current_model(self):
+        return classifier.model_id
+
+    def create_model_from_database(self, model_id):
         reviews = DbManager.get_reviews(limit=5000)
-        model_id, accuracy = classifier.create_model(reviews, model_id=asin)
+        model_id, accuracy = classifier.create_model(reviews, model_id=model_id)
         return model_id, accuracy
 
-    @classmethod
-    def _initialize_classifier(cls):
+    def load_model(self, model_id):
+        return classifier.load_model(model_id)
+
+    def remove_model(self, model_id):
+        file_to_rem = pathlib.Path(self._MODELS_DIR + "/" + model_id + ".mdl")
+        if file_to_rem.is_file():
+            file_to_rem.unlink()
+            return "ok"
+        else:
+            raise Exception("The model with id '" + model_id + "' does not exist")
+
+    def get_saved_models(self, limit=None):
+        model_ids = []
+        retrieved = 0
+        for file in glob.glob(self._MODEL_CONTAINER_REGEX):
+            if limit is not None and retrieved >= limit:
+                return model_ids
+            file_match = re.search(self._MODEL_REGEX, file)
+            model_id = file_match.group("id")
+            model_ids.append(model_id)
+            retrieved += 1
+        return model_ids
+
+    def get_last_model(self):
+        model = self.get_saved_models(1)
+        if len(model) > 0:
+            return model[0]
+        else:
+            return None
+
+    def _initialize_classifier(self):
         Logger.log_warning("initializing Classifier...")
-        if not classifier.load_first_model():
+        model = self.get_last_model()
+        if not classifier.load_model(model):
             reviews = DbManager.get_reviews(limit=5000)
             model_id, accuracy = classifier.create_model(reviews)
+            classifier.load_model(model_id)
             Logger.log_warning(model_id + "::" + accuracy)
--- gloover_ws/blueprints/classifier_bp.py
+++ gloover_ws/blueprints/classifier_bp.py
@@ -7,10 +7,41 @@ classifier_api = Blueprint('classifier_api', __name__)
 classifier_service = ClassifierService()
 
 
-@classifier_api.route('/classify')
+@classifier_api.route('/classify', methods=['PUT'])
 def classify():
     text = request.form.get('text')
     if text is None:
         raise NullRequestArgsException("text was null")
     res = classifier_service.classify([text]).tolist()
+    return jsonify(data={"text": text, "polarity": res[0]})
+
+
+@classifier_api.route('/models')
+def get_models():
+    limit = request.args.get('limit', None)
+    models = classifier_service.get_saved_models(limit)
+    return jsonify(data=models)
+
+
+@classifier_api.route('/models', methods=['POST'])
+def add_model():
+    model_id = request.form.get('asin', None)
+    model_id, accuracy = classifier_service.create_model_from_database(model_id)
+    return jsonify(data={"model_id": model_id, "accuracy": accuracy})
+
+
+@classifier_api.route('/models/<id>', methods=['DELETE'])
+def remove_model(id):
+    res = classifier_service.remove_model(id)
     return jsonify(data=res)
+
+
+@classifier_api.route('/models/<id>', methods=['PUT'])
+def load_model(id):
+    res = classifier_service.load_model(id)
+    return jsonify(data=res)
+
+
+@classifier_api.route('/models/current', methods=['GET'])
+def get_current_model():
+    return jsonify(data=classifier_service.get_current_model())
